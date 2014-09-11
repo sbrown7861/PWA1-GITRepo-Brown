917a862ca864b614100e7ba7568c5527b06e26ce
sbrown7861 <sbrown14@fullsail.edu>
2014-09-11 15:41:14 -0700

    V2 of duel using arrays
    arrays were added to the .js file and used instead of var in the
    syntax. pseudocode was added for the arrays and for how the arrays
    work. i.e what the values represent.

--- PWA1-FollowAlongFiles/Wk1/Duel_Week_1/script.js
+++ PWA1-FollowAlongFiles/Wk1/Duel_Week_1/script.js
@@ -1,7 +1,7 @@
 /*
  Skyler Brown
- Week 1  ANALYZE Duel #1
- 07/05/2014
+ Week 1  ANALYZE Duel 2
+ 09/11/2014
  */
 
 
@@ -11,7 +11,7 @@
 
     console.log("FIGHT!!!");// print "FIGHT" to console to check and make sure HTML and JS files are working together.
 
-    //player name
+  /*  //player name
     var playerOneName = "Spiderman";// name for player one as a variable for logic processes.
     var playerTwoName = "Batman";// name for player twp as a variable for logic processes.
 
@@ -23,24 +23,41 @@
     var playerOneHealth = 100;// player one health for logic processes.
     var playerTwoHealth = 100;// player two health for logic processes.
 
+        */
+
+    var playerOne = ["Spiderman","20","100" ];//array for player one info.
+
+    var playerTwo = ["Batman","20","100" ];//array for player two info.
+
+
+    /*
+    Inside arrays 0 is equal to the player name, 1 is equal to the damage they can deal,
+    and 2 is equal to the players health.
+     */
+
+
+
+
+
+
     //initiate round
     var round=0;// round number set to zero for math function i++ will make round appear as 1 in the alert.
 
     function fight(){ //this function is the function to simulate fighting and rounds.
-        alert(playerOneName+":"+playerOneHealth+"  *START*  "+playerTwoName+":"+playerTwoHealth);//print player names, life points, and round number to console making sure the files are working correctly.
+        alert(playerOne[0]+":"+playerOne[2]+"  *START*  "+playerTwo[0]+":"+playerTwo[2]);//print player names, life points, and round number to console making sure the files are working correctly.
         for (var i = 0; i < 10; i++)
         {
             //random formula is - Math.floor(Math.random() * (max - min) + min);
-            var minDamage1 = player1Damage * .5;// variable for min damage player 1 can deal 20 * .5 makes min damage 10.
-            var minDamage2 = player2Damage * .5;// variable for min damage player 2 can deal 20 * .5 makes min damage 10.
-            var f1 = Math.floor(Math.random()*(player1Damage-minDamage1)+minDamage1);// variable to determine how much damage to deal to player 1.
-            var f2 = Math.floor(Math.random()*(player2Damage-minDamage2)+minDamage2);// variable to determine how much damage to deal to player 2.
+            var minDamage1 = playerOne[1] * .5;// variable for min damage player 1 can deal 20 * .5 makes min damage 10.
+            var minDamage2 = playerTwo[1] * .5;// variable for min damage player 2 can deal 20 * .5 makes min damage 10.
+            var f1 = Math.floor(Math.random()*(playerOne[1]-minDamage1)+minDamage1);// variable to determine how much damage to deal to player 1.
+            var f2 = Math.floor(Math.random()*(playerTwo[1]-minDamage2)+minDamage2);// variable to determine how much damage to deal to player 2.
 
             //inflict damage
-            playerOneHealth-=f1;// new health reading for player 1.
-            playerTwoHealth-=f2;// new health reading for player 2.
+            playerOne[2]-=f1;// new health reading for player 1.
+            playerTwo[2]-=f2;// new health reading for player 2.
 
-            console.log(playerOneName+": "+playerOneHealth + " " + playerTwoName+":"+playerTwoHealth);//print player names, life points, and round number to console making sure the files are working correctly.
+            console.log(playerOne[0]+": "+playerOne[2] + " " + playerTwo[0]+":"+playerTwo[2]);//print player names, life points, and round number to console making sure the files are working correctly.
 
             //check for victor
             var result = winnerCheck();// variable to check winner.
@@ -48,7 +65,7 @@
             if (result==="no winner")
             {
                 round++;
-                alert(playerOneName+":"+playerOneHealth+"  *ROUND "+round+" OVER"+"*  "+playerTwoName+":"+playerTwoHealth); // alert pop-up to player informing them on player name, player health, and round number.
+                alert(playerOne[0]+":"+playerOne[2]+"  *ROUND "+round+" OVER"+"*  "+playerTwo[0]+":"+playerTwo[2]); // alert pop-up to player informing them on player name, player health, and round number.
 
             } else{
                 alert(result);// alert pop-up letting player know the results of the round.
@@ -60,14 +77,14 @@
 
     function winnerCheck(){ //this function is the winner check function and is outside the other functions
         var result="no winner";// variable for logic process if no winner occurs.
-        if (playerOneHealth<1 && playerTwoHealth<1)
+        if (playerOne[2]<1 && playerTwo[2]<1)
         {
             result = "You Both Die";
-        } else if(playerOneHealth<1){
-            result =playerTwoName+" WINS!!!"
-        } else if (playerTwoHealth<1)
+        } else if(playerOne[2]<1){
+            result =playerTwo[0]+" WINS!!!"
+        } else if (playerTwo[2]<1)
         {
-            result = playerOneName+" WINS!!!"
+            result = playerOne[0]+" WINS!!!"
         };
         return result;// command to return result from loop. if this is missing no information can be taken from the loop.
     };
