fee84745f8ecb5bce13e44ee80bfafa3b4bf01f5
sbrown7861 <sbrown14@fullsail.edu>
2014-09-19 20:02:18 -0700

    Duel v3 everything working but the button= false.
    I got everything to work except the button to return false. I cannot
    figure it out. If you could help me for the future I would appreciate
    it.

--- PWA1-FollowAlongFiles/Wk3/Fight_app_v3/script.js
+++ PWA1-FollowAlongFiles/Wk3/Fight_app_v3/script.js
@@ -21,71 +21,104 @@
 
      */
 
-    var playerOne = ["Kabal","20","100" ];//array for player one info.
+  /*  var playerOne = ["Kabal","20","100" ];//array for player one info.
 
-    var playerTwo = ["Kratos","20","100" ];//array for player two info.
+    var playerTwo = ["Kratos","20","100" ];//array for player two info. */
 
 
     /*
      Inside arrays 0 is equal to the player name, 1 is equal to the damage they can deal,
      and 2 is equal to the players health.
      */
+var fighter1_txt =document.querySelector("#kratos").querySelector("p");
+var fighter2_txt = document.querySelector("#kabal").querySelector("p");
+var round_txt = document.querySelector("h5");
+var button = document.getElementById("fight_btn");
+
+    button.addEventListener("click",fight,false);
+   var fighters =[{
+        name:"kratos",
+
+        health:"100",
+
+        attack:"20"
+
+
+    },
+
+   {
+        name:"kabal",
+
+        health:"100",
+
+        attack:"20"
+    }];
 
 
 
+    round_txt.innerHTML="Click FIGHT BUTTON to Start!";
+    fighter1_txt.innerHTML=fighters[0].name +":"+fighters[0].health;
+    fighter2_txt.innerHTML=fighters[1].name +":"+fighters[1].health;
 
 
 
     //initiate round
     var round=0;// round number set to zero for math function i++ will make round appear as 1 in the alert.
 
-    function fight(){ //this function is the function to simulate fighting and rounds.
-        alert(playerOne[0]+":"+playerOne[2]+"  *START*  "+playerTwo[0]+":"+playerTwo[2]);//print player names, life points, and round number to console making sure the files are working correctly.
-        for (var i = 0; i < 10; i++)
+    function fight(){
+
+        //alert for fighters names and health.
+        fighter1_txt.innerHTML=fighters[0].name + ":" + fighters[0].health;
+        fighter2_txt.innerHTML=fighters[1].name + ":" + fighters[1].health;
+
+        //determines damage dealt.
+        var f1 = Math.floor(Math.random() *fighters[0].attack + fighters[0].attack *.5);
+        var f2 = Math.floor(Math.random() *fighters[1].attack + fighters[1].attack *.5);
+
+        //inflicts random damage.
+        fighters[0].health -= f1;
+        fighters[1].health -= f2;
+
+        //checks for winner.
+        var results= winnerCheck();
+
+        round_txt.innerHTML = "Round #" + round + "Results:";
+        round++;
+        if (results === "No Winner")
         {
-            //random formula is - Math.floor(Math.random() * (max - min) + min);
-            var minDamage1 = playerOne[1] * .5;// variable for min damage player 1 can deal 20 * .5 makes min damage 10.
-            var minDamage2 = playerTwo[1] * .5;// variable for min damage player 2 can deal 20 * .5 makes min damage 10.
-            var f1 = Math.floor(Math.random()*(playerOne[1]-minDamage1)+minDamage1);// variable to determine how much damage to deal to player 1.
-            var f2 = Math.floor(Math.random()*(playerTwo[1]-minDamage2)+minDamage2);// variable to determine how much damage to deal to player 2.
+            fighter1_txt.innerHTML = fighters[0].name + ":" + fighters[0].health;
+            fighter2_txt.innerHTML = fighters[1].name + ":" + fighters[1].health;
+        }else {
+            fighter1_txt.innerHTML = results;
+            fighter2_txt.innerHTML = "";
 
-            //inflict damage
-            playerOne[2]-=f1;// new health reading for player 1.
-            playerTwo[2]-=f2;// new health reading for player 2.
+            //disable button.
+            button.removeEventListener("click", fight, false);
+            while(button.disabled = true) {
 
-            console.log(playerOne[0]+": "+playerOne[2] + " " + playerTwo[0]+":"+playerTwo[2]);//print player names, life points, and round number to console making sure the files are working correctly.
+                document.querySelector('buttonblue').innerHTML = "DONE!";
 
-            //check for victor
-            var result = winnerCheck();// variable to check winner.
-            console.log(result);//print result to console making sure files are working correctly.
-            if (result==="no winner")
-            {
-                round++;
-                alert(playerOne[0]+":"+playerOne[2]+"  *ROUND "+round+" OVER"+"*  "+playerTwo[0]+":"+playerTwo[2]); // alert pop-up to player informing them on player name, player health, and round number.
 
-            } else{
-                alert(result);// alert pop-up letting player know the results of the round.
-                break;//command to break out of loop.
-            };
+            }
 
-        };
-    };
+        }
 
-    function winnerCheck(){ //this function is the winner check function and is outside the other functions
-        var result="no winner";// variable for logic process if no winner occurs.
-        if (playerOne[2]<1 && playerTwo[2]<1)
+        document.querySelector('buttonblue').innerHTML = "DONE!";
+    }
+
+    function winnerCheck(){
+        var results = "No Winner";
+        if(fighters[0].health < 1 && fighters[1].health < 1)
         {
-            result = "You Both Die";
-        } else if(playerOne[2]<1){
-            result =playerTwo[0]+" WINS!!!"
-        } else if (playerTwo[2]<1)
+            results = "You Both Die!";
+        }else if(fighters[0].health < 1){
+            results= fighters[1].name + " WINS!"
+        } else if (fighters[1].health <1)
         {
-            result = playerOne[0]+" WINS!!!"
-        };
-        return result;// command to return result from loop. if this is missing no information can be taken from the loop.
-    };
+            results = fighters[0].name + " WINS!"
+        }
+       return results;
 
-    /*******  The program gets started below *******/
-    fight();//call command for the fight function.
 
-})();
+    }
+    })();
